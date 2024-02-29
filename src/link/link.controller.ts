81e25eea204fec2968cf2fce4923594721a18e44
udoaniekan <udoaniekan1995@gmail.com>
2024-02-29 15:12:52 +0100

    yea

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { LinkService } from './link.service';
 import { LinkDto } from 'src/dto/link.dto';
 
@@ -11,10 +11,10 @@ export class LinkController {
         return await this.linkService.addlink(payload)
     }
 
-     @Get(':name')
-    async findName(@Param('name') name: string) {
-        return await this.linkService.findOneByName(name);
-    }
+    //  @Get(':name')
+    // async findName(@Param('name') name: string) {
+    //     return await this.linkService.findOneByName(name);
+    // }
 
     @Get(':id')
     async findId(@Param('id') id: number) {
@@ -25,5 +25,15 @@ export class LinkController {
     @Get()
     async findAllLink(){
         return await this.linkService.findAll()
+    }
+
+    @Put(':id')
+    async updateLinkByName(@Param('id')id, @Body() payload){
+        return await this.linkService.updateLink(id, payload)
+    }
+
+    @Delete(':id')
+    async deleteByName(@Param('id')id:number){
+        return await this.linkService.deleteLink(id)
     }
 }
--- src/link/link.service.ts
+++ src/link/link.service.ts
@@ -13,13 +13,13 @@ export class LinkService {
     // or simply write:return await this.linkRepository.save(payload)
     }
 
-    async findOneByName(name:string){
-        const find = await this.linkRepository.findOne({where:{name:name}});
-        if(!find){
-            throw new HttpException('sorry no product found', 400 );
-        }
-        return find;
-    }
+    // async findOneByName(name:string){
+    //     const find = await this.linkRepository.findOne({where:{name:name}});
+    //     if(!find){
+    //         throw new HttpException('sorry no product found', 400 );
+    //     }
+    //     return find;
+    // }
 
     // find one by  id
     async findOneById(id:number){
@@ -32,8 +32,41 @@ export class LinkService {
         }
         return find;
     }
-
+    // find all link
     async findAll(){
         return await this.linkRepository.find()
     }
+
+    // update link
+    async updateLink(id, payload){
+        const update = await this.linkRepository.findOne({where:{id}})
+        if (!update){
+            throw new HttpException('sorry no product to update', 400 );
+        }
+        const newUpdate = await this.linkRepository.update(id, payload);
+
+        return{
+            statusCode: 201,
+            message: 'Link successfully updated',
+        
+        }
+    }
+    
+
+    // delete link
+    async deleteLink(id:number){
+        const find = await this.linkRepository.findOne({where:{id:id}});
+
+        // error handling
+        if(!find){
+            throw new HttpException('sorry no link found', 400 );
+        }   
+
+        await this.linkRepository.delete(id);
+        return{
+            statusCode: 200,
+            message: 'Link successfully deleted'
+        }
+    }
+
 }
